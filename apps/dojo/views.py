95b3a5089c11777febbfae48e8fd21bea4c3cbef
Andres Velasquez <andres.velasquez@me.com>
2017-03-31 01:05:42 -0700

    implemented hint system

--- apps/dojo/views.py
+++ apps/dojo/views.py
@@ -77,7 +77,7 @@ def clearSession(request):
 # questionBank was a dictionary of dictionaries before but I found that when using for key, value in template with that dictionary did not necessarily preserve the order. Iterating a list will go in order however so it's a list now
 questionBank = {
     "yellow": [ # questionNumber must be in order starting with y1 for yellow, r1 for red and b1 for black. Text key is the question itself.
-    {"questionNumber": "y1", "text": "Find all baseball leagues."},
+    {"questionNumber": "y1", "text": "Find all baseball leagues."}, # add a hint key to any question you want to have a hint button for (shown next to submit button). value will be the hint text to display in the resultsWindow to the right
     {"questionNumber": "y2", "text": "Find all womens' leagues."},
     {"questionNumber": "y3", "text": "Find all leagues where sport is any type of hockey."},
     {"questionNumber": "y4", "text": "Find all leagues where sport is something OTHER THAN football."},
