0418e73bd9252399976756abada0dedc802aeffd
Andres Velasquez <andres.velasquez@me.com>
2017-04-05 22:01:55 -0700

    made hint button permanent again unless question answered correctly

--- apps/dojo/static/dojo/js/belts.js
+++ apps/dojo/static/dojo/js/belts.js
@@ -21,10 +21,8 @@ $(document).ready(function(){
       $("#" + results["div"]).find(".resultsWindow").html(results["resultsWindow"]);
       if(results["smiley"]){ // check if there's a smiley in the results that needs to be rendered
         $("#" + results["div"]).find(".smiley").html("<img src='/static/dojo/images/smiley.png' alt='smiley'>");
-        $("#" + results["div"]).find(".hintButton").hide(); // hide hint button after right answer is given
       } else { // if smiley not present in response, delete it from the smiley div because wrong answer was submitted
         $("#" + results["div"]).find(".smiley").html("");
-        $("#" + results["div"]).find(".hintButton").show(); // show hint button if incorrect answer is given
       }
     });
   }
